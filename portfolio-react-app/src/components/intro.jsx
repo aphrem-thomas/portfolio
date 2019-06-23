57db1c2fac9b48e27f64119b887a016c2e3522b3
aphremthomas <aphremthomas@gmail.com>
2019-06-23 19:51:11 +0530

    hi its aphrem

--- portfolio-react-app/src/components/intro.jsx
+++ portfolio-react-app/src/components/intro.jsx
@@ -1,17 +1,17 @@
 import React from 'react';
-import aphrem1 from "../images/aphrem1.jpg";
-
-class Intro extends React.Component{
-    render(){
-        return(
-            <div className="container">
-            <div className="introbox jumbotron">
-            <span className="intro-text">
-                Experienced Software Engineer with a demonstrated history of working in the information technology and services industry. Skilled in Java, JavaScript, React.js, React Native, and Adobe Photoshop. Strong engineering professional with a Master of Technology (MTech) focused in Computer Science from College of Engineering, Trivandrum.            
-            </span>         
-            <img src={aphrem1} className="profilepic"/>   
+import aphremPhoto from "../images/aphrem-BlackBG.jpg";
+import styles from './intro.css';
+class Intro extends React.Component {
+    render() {
+        return (
+            <div className={"wrapper"}>
+                <div className="profilePhoto"><img src={aphremPhoto} /></div>
+                <div className="intro">
+                    <div className="intro-wrapper">
+                        <div className="hi">Hi</div>
+                        <div className="aphremName">Aphrèm</div>
+                </div>
             </div>
-
             </div>
         );
     }
