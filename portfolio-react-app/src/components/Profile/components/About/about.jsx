74bb06d6bef132e3450c6d36893181ce94466870
APHREM THOMAS <aphremthomas@macbooks-MacBook-Pro-3.local>
2019-09-10 22:06:04 +0530

    width corrected

--- portfolio-react-app/src/components/Profile/components/About/about.jsx
+++ portfolio-react-app/src/components/Profile/components/About/about.jsx
@@ -3,13 +3,14 @@ import aphremPhoto from '../../../../images/aphrem1.jpg';
 import './about.css';
 import Skills from '../Skills/skills';
 import ProfileCard from '../ProfileCard/ProfileCard';
+import ProfileInfo from '../ProfileInfo/ProfileInfo';
 
 const About = ()=>{
     return(
             <div id="about-page" className="about-wrapper">
                 <ProfileCard/>
+                <ProfileInfo/>
                 <span className="about-info">
-                    {`Proactive and experienced Software Engineer with 2.8 years of demonstrated history in Front end application development. Skilled in JavaScript, React-JS, HTML, CSS. Strong engineering professional with a Master’s degree (M Tech) in Computer Science & Engineering from the College of Engineering, Trivandrum.`}
                 </span>
             </div>
     );
---
+++ portfolio-react-app/src/components/Profile/components/ProfileInfo/ProfileInfo.jsx
@@ -0,0 +1,9 @@
+import React from 'react';
+import './ProfileInfo.css';
+export default function ProfileInfo() {
+    return (
+        <div className="profileInfo-wrapper">
+            <div className="profileInfo-content">{`Proactive and experienced Software Engineer with 2.8 years of demonstrated history in Front end application development. Skilled in JavaScript, React-JS, HTML, CSS. Strong engineering professional with a Master’s degree (M Tech) in Computer Science & Engineering from the College of Engineering, Trivandrum.`}</div>
+        </div>
+    )
+}
