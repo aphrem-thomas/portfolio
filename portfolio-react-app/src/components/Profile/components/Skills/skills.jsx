36e404e7e14951e1e776c902cf0e25fddf7dae93
APHREM THOMAS <aphremthomas@macbooks-MacBook-Pro-3.local>
2019-09-19 11:10:03 +0530

    more skills added

--- portfolio-react-app/src/components/Profile/components/Skills/skills.jsx
+++ portfolio-react-app/src/components/Profile/components/Skills/skills.jsx
@@ -1,7 +1,13 @@
 import React from 'react';
-import reactLogo from '../../../../images/logos/logo.svg'
-import js from '../../../../images/logos/js.svg'
-import node from '../../../../images/logos/node.svg'
+import reactLogo from '../../../../images/logos/logo.svg';
+import js from '../../../../images/logos/js.svg';
+import node from '../../../../images/logos/node.svg';
+import nginx from '../../../../images/logos/nginx.svg';
+import css3 from '../../../../images/logos/css3.svg';
+import html5 from '../../../../images/logos/html.svg';
+import reduxlogo from '../../../../images/logos/redux.svg';
+
+
 
 import aphremPhoto from "../../../../images/aphrem-BlackBG.jpg";
 import './skills.css'
@@ -19,6 +25,18 @@ const Skills = () => {
            </div>
            <div className="skills-indiv">
             <LangSkill name="Node JS" logo={node} value={70} color={'blue'}/>
+            </div>
+            <div className="skills-indiv">
+            <LangSkill name="HTML5" logo={html5} value={90} color={'green'}/>
+            </div>
+            <div className="skills-indiv">
+            <LangSkill name="CSS3" logo={css3} value={90} color={'green'}/>
+            </div>
+            <div className="skills-indiv">
+            <LangSkill name="redux" logo={reduxlogo} value={90} color={'blue'}/>
+            </div>
+            <div className="skills-indiv">
+            <LangSkill name="nginx" logo={nginx} value={50} color={'blue'}/>
             </div>
           </div>
         </div>
