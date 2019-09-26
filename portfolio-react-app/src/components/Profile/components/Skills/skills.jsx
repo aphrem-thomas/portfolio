4d10b84cd5315ec9142e15fdf5e61679ab435f5f
APHREM THOMAS <aphremthomas@macbooks-MacBook-Pro-3.local>
2019-09-26 21:56:01 +0530

    sills added

--- portfolio-react-app/src/components/Profile/components/Skills/skills.jsx
+++ portfolio-react-app/src/components/Profile/components/Skills/skills.jsx
@@ -6,10 +6,13 @@ import nginx from '../../../../images/logos/nginx.svg';
 import css3 from '../../../../images/logos/css3.svg';
 import html5 from '../../../../images/logos/html.svg';
 import reduxlogo from '../../../../images/logos/redux.svg';
-
-
-
+import webpack from '../../../../images/logos/webpack.svg';
+import jest from '../../../../images/logos/jest.svg';
+import sass from '../../../../images/logos/sass.svg';
+import immutable from '../../../../images/logos/immutable.svg';
+import d3 from '../../../../images/logos/d3.svg';
 import aphremPhoto from "../../../../images/aphrem-BlackBG.jpg";
+
 import './skills.css'
 import LangSkill from '../LangSkill/LangSkill';
 const Skills = () => {
@@ -33,10 +36,25 @@ const Skills = () => {
             <LangSkill name="CSS3" logo={css3} value={90} color={'green'}/>
             </div>
             <div className="skills-indiv">
-            <LangSkill name="redux" logo={reduxlogo} value={90} color={'blue'}/>
+            <LangSkill name="redux" logo={reduxlogo} value={90} color={'green'}/>
             </div>
             <div className="skills-indiv">
             <LangSkill name="nginx" logo={nginx} value={50} color={'blue'}/>
+            </div>
+            <div className="skills-indiv">
+            <LangSkill name="Webpack" logo={webpack} value={50} color={'blue'}/>
+            </div>
+            <div className="skills-indiv">
+            <LangSkill name="Jest" logo={jest} value={60} color={'yellow'}/>
+            </div>
+            <div className="skills-indiv">
+            <LangSkill name="Sass" logo={sass} value={60} color={'yellow'}/>
+            </div>
+            <div className="skills-indiv">
+            <LangSkill name="Immutable JS" logo={immutable} value={60} color={'yellow'}/>
+            </div>
+            <div className="skills-indiv">
+            <LangSkill name="D3" logo={d3} value={60} color={'blue'}/>
             </div>
           </div>
         </div>
