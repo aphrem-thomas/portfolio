8779922ee354a3c21fe3b45c60ac7bcdd98b4c7f
APHREM THOMAS <aphremthomas@macbooks-MacBook-Pro-3.local>
2019-09-27 10:48:08 +0530

    github and resume download link added

--- portfolio-react-app/src/components/Profile/components/ProfileCard/ProfileCard.jsx
+++ portfolio-react-app/src/components/Profile/components/ProfileCard/ProfileCard.jsx
@@ -13,16 +13,20 @@ export default function ProfileCard() {
                 <h2>Aphrem Thomas</h2>
                 <h1>Software Developer</h1>
                 <div className="profile-card-experience">
-                    Worked in Accenture for 2 years as Javascript Developer (React), currently
-                    working as Front-End Develper in Razorthink technologies Bangalore from January 2019.
+                    Worked in Accenture for 2 years (from Jan 2017) as Javascript Developer (React), currently
+                    working as Front-End Developer in Razorthink technologies Bangalore from January 2019.
                 </div>
+                <a href="https://github.com/aphrem-thomas" target="_blank">
                 <div className="github">
                     <img className="github-logo" src={github}/>
                     <h3>github</h3>
                 </div>
-                <div className="resume">
-                    <h3>resumé...</h3>
-                </div>
+                </a>
+                <a href="https://drive.google.com/file/d/1Bx8gHn77s1wnBzbEkfyteMXM2kXJRl6t/view?usp=sharing" download>
+                    <div className="resume">
+                        <h3>resumé...</h3>
+                    </div>
+                </a>
             </div>
         </div>
     )
